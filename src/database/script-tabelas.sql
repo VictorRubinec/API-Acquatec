f0043f6d45c75cdc787e13edbd5ef7981db052c9
VictorRubinec <victor.rubinec@sptech.school>
2022-04-18 14:00:12 -0300

    API - campo cpf

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -11,7 +11,8 @@ CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
+	senha VARCHAR(50),
+	cpf CHAR(11)
 );
 
 CREATE TABLE aviso (
