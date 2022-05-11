16a6ba377c6f6ca03fbf6b5f5c29260cb2368be2
VictorRubinec <victor.rubinec@sptec.school>
2022-05-11 12:56:28 -0300

    conexão azure - login

--- src/database/config.js
+++ src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    user: "XXXXX",
-    password: "XXXXX",
-    database: "XXXXX",
-    server: "XXXXX",
+    user: "grupo2-1cco",
+    password: "#Gfsentineltrack",
+    database: "sentineltrack",
+    server: "grupo2-1cco.database.windows.net",
     pool: {
         max: 10,
         min: 0,
